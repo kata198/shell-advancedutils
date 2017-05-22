da12cfb43cfa35fea0f2cf894de707d4583bcb4e
Tim Savannah <kata198@gmail.com>
2017-05-22 18:01:58 -0400

    Add my allocated_str impl for use

---
+++ src/allocated_str.c
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2017 Timothy Savannah All Rights Reserved
+ *
+ *  Licensed under terms of the LGPLv3
+ */
+
+#include "common.h"
+#include "allocated_str.h"
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ALLOCATED_STRING_MIN_SIZE ( 255 )
+
+
+STATIC_INLINE_WHEN_STATIC void astr_grow(AllocatedStr *self, int doRealloc)
+{
+    size_t newSize;
+
+    newSize = self->size * self->growRate;
+    if ( unlikely( newSize < ALLOCATED_STRING_MIN_SIZE ) )
+    {
+        newSize = ALLOCATED_STRING_MIN_SIZE;
+    }
+
+    if( unlikely( doRealloc ) && likely ( self->value != NULL ) )
+    {
+        self->value = realloc(self->value, newSize);
+    }
+    else
+    {
+        self->value = malloc(newSize);
+        *(self->value) = '\0';
+    }
+
+    self->size = newSize;
+}
+
+
+STATIC_INLINE_WHEN_STATIC AllocatedStr *astr_new(size_t initialSize, float growRate)
+{
+    AllocatedStr *ret;
+
+    if ( growRate <= 0 )
+        growRate = ALLOCATED_STR_DEFAULT_GROW_RATE;
+
+    ret = calloc(1, sizeof(AllocatedStr));
+
+    if ( initialSize >= 0 )
+    {
+        if ( initialSize < ALLOCATED_STRING_MIN_SIZE )
+            initialSize = ALLOCATED_STRING_MIN_SIZE;
+
+        ret->value = malloc(initialSize);
+        ret->size = initialSize;
+
+        *(ret->value) = '\0';
+    }
+    ret->growRate = growRate;
+
+    return ret;
+}
+
+STATIC_INLINE_WHEN_STATIC void astr_free(AllocatedStr *self)
+{
+    if ( likely( self->value != (char*)0 ) )
+        free(self->value);
+
+    free(self);
+}
+
+
---
+++ src/allocated_str.h
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2017 Timothy Savannah All Rights Reserved
+ *
+ *  Licensed under terms of the LGPLv3
+ *
+ *  allocated_str.h - Header for AllocatedStr functions.
+ *    Define ALLOCATED_STR_STATIC_INLINE to make everything static and 
+ *    include allocated_str.c into file. If multiple files and you want
+ *    this, consider making a use_allocated_str.c that just has the include,
+ *    compiling into an object and linking to final binary
+ */
+#ifndef __ALLOCATED_STR_H
+#define __ALLOCATED_STR_H
+
+#ifdef ALLOCATED_STR_STATIC_INCLUDE
+  #define STATIC_INLINE_WHEN_STATIC static inline
+#else
+  #define STATIC_INLINE_WHEN_STATIC
+#endif
+
+#define ALLOCATED_STR_DEFAULT_GROW_RATE 2
+
+typedef struct {
+    char *value;
+    size_t size;
+    float growRate;
+} AllocatedStr;
+
+#define ASTR_VALUE(_a_str) (_a_str->value)
+#define ASTR_SIZE(_a_str)  (_a_str->size)
+
+#define ASTR_LEN_OK(_a_str, _len) ( ASTR_SIZE(_a_str) >= _len )
+
+STATIC_INLINE_WHEN_STATIC AllocatedStr *astr_new(size_t initialSize, float growRate);
+
+STATIC_INLINE_WHEN_STATIC void astr_free(AllocatedStr *self);
+
+STATIC_INLINE_WHEN_STATIC void astr_grow(AllocatedStr *self, int doRealloc);
+
+static inline void astr_ensureSize(AllocatedStr *self, size_t newSize, int doRealloc)
+{
+    while ( ! ASTR_LEN_OK(self, newSize) )
+    {
+        astr_grow(self, doRealloc);
+    }
+}
+
+
+#ifdef ALLOCATED_STR_STATIC_INCLUDE
+#include "allocated_str.c"
+#endif
+
+#endif
