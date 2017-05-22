ff1b1bf0319eca934b65ba5676243f992cfbb675
Tim Savannah <kata198@gmail.com>
2017-05-22 18:02:03 -0400

    Fix common.h

--- src/common.h
+++ src/common.h
@@ -4,8 +4,8 @@
 
 #ifdef __GNUC__
 
-#define likely   (x) (__builtin_expect( !!(x) , 1))
-#define unlikely (x) (__builtin_expect( !!(x) , 0))
+#define likely(x) (__builtin_expect( !!(x) , 1))
+#define unlikely(x) (__builtin_expect( !!(x) , 0))
 
 #else
 
